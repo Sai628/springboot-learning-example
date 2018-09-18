30e4309beba59047ae670b8233600259c62cc444
Sai628 <sai100628@gmail.com>
2018-09-18 19:20:21 +0800

    add chapter 3 - spring boot web

---
+++ chapter-3-spring-boot-web/src/main/java/demo/springboot/service/impl/BookServiceImpl.java
@@ -0,0 +1,60 @@
+package demo.springboot.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import demo.springboot.domain.Book;
+import demo.springboot.service.BookService;
+
+
+/**
+ * Book 业务实现层
+ */
+@Service
+public class BookServiceImpl implements BookService
+{
+    // 模拟数据库, 存储 Book 信息
+    private static Map<Long, Book> BOOK_DB = new HashMap<>();
+
+
+    @Override
+    public List<Book> findAll()
+    {
+        return new ArrayList<>(BOOK_DB.values());
+    }
+
+
+    @Override
+    public Book insertByBook(Book book)
+    {
+        book.setId(BOOK_DB.size() + 1L);
+        BOOK_DB.put(book.getId(), book);
+        return book;
+    }
+
+
+    @Override
+    public Book update(Book book)
+    {
+        BOOK_DB.put(book.getId(), book);
+        return book;
+    }
+
+
+    @Override
+    public Book delete(Long id)
+    {
+        return BOOK_DB.remove(id);
+    }
+
+
+    @Override
+    public Book findById(Long id)
+    {
+        return BOOK_DB.get(id);
+    }
+}
